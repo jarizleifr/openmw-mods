b52f3691b1745d5b6ca30e53f8616892683f4278
jarizleifr <antti.joutsi@gmail.com>
2023-07-07 00:49:41 +0300

    BasicNeeds 1.1.1: Refactoring, code cleanup

--- BasicNeeds/scripts/BasicNeeds/player.lua
+++ BasicNeeds/scripts/BasicNeeds/player.lua
@@ -3,69 +3,31 @@
 -- scripts/BasicNeeds/player.lua
 -- 2023 -- Antti Joutsi <antti.joutsi@gmail.com>
 -- -----------------------------------------------------------------------------
-local math = require("math")
 local async = require("openmw.async")
 local core = require("openmw.core")
 local input = require("openmw.input")
 local self = require("openmw.self")
-local types = require("openmw.types")
 local ui = require("openmw.ui")
 local time = require("openmw_aux.time")
 
-local hud = require("scripts.BasicNeeds.hud")
+local Actor = require("openmw.types").Actor
+
 local settings = require("scripts.BasicNeeds.settings")
 local bed = require("scripts.BasicNeeds.bed")
+
+local Need = require("scripts.BasicNeeds.need")
+local STATE = Need.STATE
 
 local L = core.l10n("BasicNeeds")
 
 -- -----------------------------------------------------------------------------
--- Constants
--- -----------------------------------------------------------------------------
-local INIT = 0
-local NONE = 1
-local MILD = 2
-local MODERATE = 3
-local SEVERE = 4
-local CRITICAL = 5
-local DEATH = 6
-
-local UPDATE_INTERVAL = time.second * 10
-
--- -----------------------------------------------------------------------------
 -- Script state
 -- -----------------------------------------------------------------------------
-local function createState(key, value)
-   return {
-      value = value,
-      widget = hud[key],
-      effects = {
-         [MILD]     = "jz_mild_" .. key,
-         [MODERATE] = "jz_moderate_" .. key,
-         [SEVERE]   = "jz_severe_" .. key,
-         [CRITICAL] = "jz_critical_" .. key,
-      },
-      messages = {
-         increase = {
-            [MILD]     = L(key .. "IncreaseMild"),
-            [MODERATE] = L(key .. "IncreaseModerate"),
-            [SEVERE]   = L(key .. "IncreaseSevere"),
-            [CRITICAL] = L(key .. "IncreaseCritical"),
-            [DEATH]    = L(key .. "IncreaseDeath"),
-         },
-         decrease = {
-            [NONE]     = L(key .. "DecreaseNone"),
-            [MILD]     = L(key .. "DecreaseMild"),
-            [MODERATE] = L(key .. "DecreaseModerate"),
-            [SEVERE]   = L(key .. "DecreaseSevere"),
-            [CRITICAL] = L(key .. "DecreaseCritical"),
-         }
-      },
-   }
-end
+local UPDATE_INTERVAL = time.second * 10
 
-local thirst = createState("thirst", 0)
-local hunger = createState("hunger", 0)
-local exhaustion = createState("exhaustion", 0)
+local thirst = Need.create("thirst", self, 0)
+local hunger = Need.create("hunger", self, 0)
+local exhaustion = Need.create("exhaustion", self, 0)
 
 local previousTime = nil
 local previousCell = nil
@@ -73,56 +35,17 @@ local previousCell = nil
 local wellRestedTime = nil
 local sleepingInBed = false
 
-local maxValue = nil
 local thirstRate = nil
 local hungerRate = nil
 local exhaustionRate = nil
 local exhaustionRecoveryRate = nil
 
-local function getStatus(value)
-   return 1 + math.floor(value / 200)
-end
-
-local function updateEffects(prevStatus, need)
-   local status = getStatus(need.value)
-   if (status ~= prevStatus) then
-      for _, effect in pairs(need.effects) do
-         types.Actor.spells(self):remove(effect)
-      end
-      if (status == DEATH) then
-         local health = types.Actor.stats.dynamic.health(self)
-         health.current = -1000
-      elseif (status > NONE) then
-         if (status > prevStatus and prevStatus ~= INIT) then
-            ui.showMessage(need.messages.increase[status])
-         end
-         types.Actor.spells(self):add(need.effects[status])
-      end
-      need.widget.layout.props.textColor = hud.colors[status]
-      need.widget:update()
-   end
-end
-
-local function updateNeed(need, change)
-   if (change == 0) then return end
-
-   local prevStatus = getStatus(need.value)
-   if (change < 0) then
-      need.value = math.max(0, need.value + change)
-      -- Show message on all value decrements to keep track where we are
-      ui.showMessage(need.messages.decrease[getStatus(need.value)])
-   else
-      need.value = math.min(maxValue, need.value + change)
-   end
-   updateEffects(prevStatus, need)
-end
-
 local function updateNeeds()
    local nextTime = core.getGameTime()
    local passedTime = nextTime - previousTime
 
-   updateNeed(thirst, thirstRate * passedTime)
-   updateNeed(hunger, hungerRate * passedTime)
+   thirst:mod(thirstRate * passedTime)
+   hunger:mod(hungerRate * passedTime)
 
    local currentCell = self.object.cell
    if (passedTime >= time.hour and previousCell == currentCell) then
@@ -130,16 +53,16 @@ local function updateNeeds()
       if (sleepingInBed and passedTime >= time.hour * 7) then
          -- Add Well Rested if rested at least 7 hours in bed
          wellRestedTime = nextTime
-         types.Actor.spells(self):add("jz_well_rested")
+         Actor.spells(self):add("jz_well_rested")
       end
-      updateNeed(exhaustion, exhaustionRecoveryRate * restMult * passedTime)
+      exhaustion:mod(exhaustionRecoveryRate * restMult * passedTime)
    else
-      updateNeed(exhaustion, exhaustionRate * passedTime)
+      exhaustion:mod(exhaustionRate * passedTime)
    end
 
    -- Remove Well Rested if 8 hours has passed
    if (wellRestedTime and nextTime - wellRestedTime >= time.hour * 8) then
-      types.Actor.spells(self):remove("jz_well_rested")
+      Actor.spells(self):remove("jz_well_rested")
       wellRestedTime = nil
    end
 
@@ -153,7 +76,11 @@ end
 -- -----------------------------------------------------------------------------
 local function loadSettings()
    -- If death is disabled, simply limit values to 999
-   maxValue = settings:get("EnableDeath") and 1000 or 999
+   local maxValue = settings:get("EnableDeath") and 1000 or 999
+   thirst:setMaxValue(maxValue)
+   hunger:setMaxValue(maxValue)
+   exhaustion:setMaxValue(maxValue)
+
    -- All rates are configured as per hour values, so we first convert them to
    -- per second values
    thirstRate = settings:get("ThirstRate") / time.hour
@@ -167,9 +94,9 @@ local function initialize(startTime)
    previousCell = self.object.cell
    loadSettings()
    -- Force update effects on initialize
-   updateEffects(INIT, thirst)
-   updateEffects(INIT, hunger)
-   updateEffects(INIT, exhaustion)
+   thirst:updateEffects(STATE.Init)
+   hunger:updateEffects(STATE.Init)
+   exhaustion:updateEffects(STATE.Init)
 end
 
 initialize(core.getGameTime())
@@ -180,9 +107,9 @@ time.runRepeatedly(updateNeeds, UPDATE_INTERVAL, { type = time.GameTime })
 -- Engine/event handlers
 -- -----------------------------------------------------------------------------
 local function onLoad(data)
-   thirst = createState("thirst", data.thirst)
-   hunger = createState("hunger", data.hunger)
-   exhaustion = createState("exhaustion", data.exhaustion)
+   thirst = Need.create("thirst", self, data.thirst)
+   hunger = Need.create("hunger", self, data.hunger)
+   exhaustion = Need.create("exhaustion", self, data.exhaustion)
    wellRestedTime = data.wellRestedTime
    initialize(data.previousTime)
    ui.updateAll()
@@ -190,7 +117,7 @@ local function onLoad(data)
    -- left in some limbo state where they can be used, but don't result in
    -- correct 'onConsume' events. By running `getAll()` on player inventory,
    -- the potions get normalized. Made issue #7448 about this.
-   types.Actor.inventory(self):getAll()
+   Actor.inventory(self):getAll()
 end
 
 local function onSave()
@@ -214,7 +141,7 @@ local function onInputAction(action)
    if (core.isWorldPaused()) then return end
    -- TODO: Using Sneak as hotkey is a workaround. Re-examine this if/when
    -- OpenMW Lua makes running on-use scripts on miscellaneous items possible
-   if (action == input.ACTION.Sneak and types.Actor.isSwimming(self)) then
+   if (action == input.ACTION.Sneak and Actor.isSwimming(self)) then
       core.sendGlobalEvent("PlayerFillContainer", {
          player = self,
       })
@@ -227,9 +154,9 @@ local function onInputAction(action)
 end
 
 local function playerConsumedFood(eventData)
-   updateNeed(thirst, eventData.thirst)
-   updateNeed(hunger, eventData.hunger)
-   updateNeed(exhaustion, eventData.exhaustion)
+   thirst:mod(eventData.thirst)
+   hunger:mod(eventData.hunger)
+   exhaustion:mod(eventData.exhaustion)
 end
 
 local function playerFilledContainer(eventData)
@@ -245,13 +172,13 @@ return {
    interface = {
       version = 1,
       getThirstStatus = function()
-         return getStatus(thirst.value)
+         return thirst:status()
       end,
       getHungerStatus = function()
-         return getStatus(hunger.value)
+         return hunger:status()
       end,
       getExhaustionStatus = function()
-         return getStatus(exhaustion.value)
+         return exhaustion:status()
       end,
    },
    engineHandlers = {
